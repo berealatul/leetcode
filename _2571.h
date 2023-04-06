44b0f64cecc817feabf87f77582d650614a839a1
berealatul <berealatul@gmail.com>
2023-04-06 16:46:54 +0530

    2571. Minimum Operations to Reduce an Integer to 0

--- _2571.h
+++ _2571.h
@@ -2,6 +2,17 @@
 class Solution {
 public:
     int minOperations(int n) {
+        if (n == 1 || n == 0) return n;
+        int val = 1;
+        while (val < n) val <<= 1;
+
+        return 1 + min(minOperations(val - n), minOperations(n - (val >> 1)));
+    }
+};
+
+class Solution {
+public:
+    int minOperations(int n) {
         int count = 0;
         while (n) {
             if ((n & 1) && ((n >> 1) & 1)) {

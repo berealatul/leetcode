1bde9782b86cfd8c97f215b46070ce0a91c23d78
berealatul <berealatul@gmail.com>
2023-04-07 08:07:58 +0530

    1020. Number of Enclaves

--- _1020.h
+++ _1020.h
@@ -9,23 +9,21 @@ public:
         for (int i = 0, n = grid.size(); i < n; i++) {
             for (int j = 0, m = grid[0].size(); j < m; j++) {
                 if (grid[i][j] == 1) {
-                    bool edgeTouched = false;
                     int tempCount = 0;
-                    dfs(i, j, grid, edgeTouched, tempCount);
-                    if (edgeTouched) tempCount = 0;
-                    count += tempCount;
+                    dfs(i, j, grid, tempCount);
+                    if (tempCount > 0) count += tempCount;
                 }
             }
         }
         return count;
     }
 private:
-    void dfs(int i, int j, vector<vector<int>>& grid, bool& edgeTouched, int& count) {
+    void dfs(int i, int j, vector<vector<int>>& grid, int& count) {
         int m = grid.size(), n = grid[0].size();
         if (i < 0 || j < 0 || i >= m || j >= n || !grid[i][j]) return;
         
         
-        if (!(i * j) || i == m - 1 || j == n - 1) edgeTouched = true;
+        if (!(i * j) || i == m - 1 || j == n - 1) count = INT_MIN;
         grid[i][j] = 0;
         count++;
 
@@ -34,7 +32,7 @@ private:
         const short y[directions] = { 0, 0, 1, -1 };
 
         for (short k = 0; k < directions; k++) {
-            dfs(i + x[k], j + y[k], grid, edgeTouched, count);
+            dfs(i + x[k], j + y[k], grid, count);
         }
     }
 };

1f43c1bcb48696c7987a08fc8fdea3b38c89c318
berealatul <berealatul@gmail.com>
2023-04-09 00:56:36 +0530

    733. Flood Fill

---
+++ _733.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        oldColor = image[sr][sc];
+        newColor = color;
+
+        dfs(sr, sc, image);
+
+        return image;
+    }
+private:
+    int oldColor, newColor;
+    void dfs(int i, int j, vector<vector<int>>& grid) {
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] != oldColor) return;
+
+        grid[i][j] = newColor;
+
+        const short directions = 4;
+        const short x[directions] = { 1, -1, 0, 0 };
+        const short y[directions] = { 0, 0, 1, -1 };
+        for (short k = 0; k < directions; k++) {
+            dfs(i + x[k], j + y[k], grid);
+        }
+    }
+};

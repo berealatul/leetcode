521fada01da6142e6ec4d80a6888f84268d86d14
berealatul <berealatul@gmail.com>
2023-04-10 15:27:57 +0530

    704. Binary Search

--- _704.h
+++ _704.h
@@ -6,21 +6,17 @@ using namespace std;
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size() - 1;
+        int start = 0, end = nums.size() - 1;
+
+        int middle;
         while (start <= end) {
-            int middle = start + ((end - start) / 2);
-            
-            if (middle == start || middle == end) {
-                if (nums[start] == target) return start;
-                if (nums[end] == target) return end;
-                break;
-            }
+            middle = (start + end) / 2;
 
             if (nums[middle] == target) return middle;
-            if (target > nums[middle]) start = middle;
-            if (target < nums[middle]) end = middle;
+            if (nums[middle] < target) start = middle + 1;
+            else end = middle - 1;
         }
+
         return -1;
     }
 };

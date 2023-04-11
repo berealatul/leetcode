e4695f4da541845cb4bfd5597ab39a1655f51435
berealatul <berealatul@gmail.com>
2023-04-11 16:22:33 +0530

    392. Is Subsequence

---
+++ _392.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m = s.size(), n = t.size();
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+            j++;
+        }
+        return i == m;
+    }
+};
